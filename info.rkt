c1a36134863b356b1dee976f26500dfb4e926203
Shawn Wagner <shawnw.mobile@gmail.com>
2023-03-25 22:23:44 -0700

    Use a different category for documentation

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 (define collection "aspell")
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/aspell.scrbl" () ("Tool Libraries"))))
+(define scribblings '(("scribblings/aspell.scrbl" () (tool-library))))
 (define pkg-desc "Interface to the GNU aspell spell checker")
 (define version "0.0")
 (define pkg-authors '(shawnw))
